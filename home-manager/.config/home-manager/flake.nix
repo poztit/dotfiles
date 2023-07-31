af763886d7969fe9d156554df0ffc2f309d6fa38
François Illien <francois@illien.org>
2023-07-31 17:48:42 +0200

    Move ZSH configuration to Nix home manager

---
+++ home-manager/.config/home-manager/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Home Manager configuration of fillien";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs = { nixpkgs, home-manager, emacs-overlay, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ emacs-overlay.overlay ];
+        config.allowUnfree = true;
+      };
+    in {
+      homeConfigurations."fillien" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
---
+++ home-manager/.config/home-manager/home.nix
@@ -0,0 +1,150 @@
+{ lib, config, pkgs, ... }:
+
+let
+  mkTuple = lib.hm.gvariant.mkTuple;
+in {
+   
+  home = {
+    username = "fillien";
+    homeDirectory = "/home/fillien";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "22.11"; # Please read the comment before changing.
+    sessionVariables = {
+      EDITOR = "emacs";
+      BROWSER = "/usr/bin/firefox";
+      MOZ_ENABLE_WAYLAND = 1;
+    };
+    shellAliases = {
+      # Replace ls by exa
+      ls = "exa --sort type --classify";
+      l = "exa --sort type --classify";
+      ll = "exa --long --icons --sort type --classify";
+      lll = "exa --long --icons --all --sort type --classify";
+      la = "exa --all --sort type --classify";
+      lla = "exa --long --icons --all --sort type --classify";
+      lt = "exa --tree --sort type";
+
+      # Cat with syntax highlighting
+      ccat = "pygmentize -g -O style=stata-dark,linenos=1";
+
+      # Use Neovim in place of Vim
+      vim = "nvim";
+    };
+  };
+  
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    fira
+    source-code-pro
+    source-sans-pro
+    (nerdfonts.override { fonts = ["FiraCode" "FantasqueSansMono"]; })
+
+    exa
+    gnome.gnome-settings-daemon
+    gnome.gnome-backgrounds
+    gnome.gnome-clocks
+    gnome.gnome-weather
+    gnomeExtensions.appindicator
+    htop
+    nvme-cli
+    python39Packages.pygments
+    ripgrep
+    stow
+    sublime-merge
+    vlc
+    mattermost-desktop
+    clang-tools
+    calls
+    libreoffice-fresh
+    evince
+    node2nix
+    pass
+  ];
+
+  fonts.fontconfig.enable = true;
+  targets.genericLinux.enable = true;
+
+  imports = [
+    ./programs/aerc.nix
+    ./accounts/email.nix
+  ];
+
+  programs = {
+    home-manager.enable = true;    
+    #firefox = {
+    #  enable = true;
+    #  package = pkgs.firefox-wayland;
+    #  profiles."fillien" = {
+    #  	settings = {
+	  #      "layout.css.prefers-color-scheme.content-override" = 0;
+	  #      "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+	  #      "intl.locale.requested" = "fr,en-US";
+	  #      "signon.rememberSignons" = false;
+	  #      "browser.startup.page" = 3;
+	  #      "browser.newtabpage.activity-stream.feeds.topsites" = false;
+	  #      "extensions.formautofill.creditCards.enabled" = false;
+	  #    };
+    #  };
+    #};
+    
+    emacs = {
+      enable = true;
+      package = pkgs.emacs-pgtk;
+    };
+    git = {
+      enable = true;
+      userName = "François Illien";
+      userEmail = "francois@illien.org";
+    };
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    direnv.enable = true;
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      syntaxHighlighting.enable = true;
+      "oh-my-zsh" = {
+        enable = true;
+        plugins = ["history"];
+      };
+      localVariables = {
+        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=#a89984";
+        LESS_TERMCAP_mb = "$'\E[01;31m'";
+        LESS_TERMCAP_md = "$'\E[01;31m'";
+        LESS_TERMCAP_me = "$'\E[0m'";
+        LESS_TERMCAP_se = "$'\E[0m'";
+        LESS_TERMCAP_so = "$'\E[01;44;33m'";
+        LESS_TERMCAP_ue = "$'\E[0m'";
+        LESS_TERMCAP_us = "$'\E[01;32m'";
+      };
+    };
+  };
+  
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      clock-show-weekday = true;
+      clock-show-date = true;
+      clock-show-seconds = true;
+    };
+    "org/gnome/desktop/calendar" = { show-weekdate = true; };
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-schedule-automatic = true;
+    };
+    "org/gnome/desktop/input-sources" = {
+      sources = [(mkTuple ["xkb" "fr+us"])];
+    };
+  };
+
+  services.emacs.enable = true;
+}
