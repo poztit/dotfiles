0db0857a4c40a8492f9074ae9f9c05b9e7628974
François Illien <francois@illien.org>
2023-08-13 21:33:00 +0200

    test

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/sh
 
+sudo apt-get install curl
+
 # Install Nix
 curl -L https://nixos.org/nix/install -o install.sh
 sh install.sh
 
 # Enable flakes
+mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
