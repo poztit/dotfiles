34eb6ea3d721fde0996f610010157c8c32011d65
François Illien <francois@illien.org>
2023-08-13 21:59:50 +0200

    sdfsfdfs

--- README.md
+++ README.md
@@ -6,4 +6,12 @@ sudo apt update && sudo apt install curl stow
 sh <(curl -L https://nixos.org/nix/install) --daemon
 ```
 
+Enable flakes :
+```bash
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+```
+
+Logout and login again to finish Nix install.
+
 
--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 # Install Nix
 curl -L https://nixos.org/nix/install -o install.sh
-sh install.sh
+curl -L https://github.com/poztit/dotfiles/archive/refs/heads/master.zip -o master.zip
+unzip master.zip
 
-# Enable flakes
-mkdir -p ~/.config/nix
-echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+stow --dir master -S home-manager emacs
+
+nix run home-manager/master -- init --switch ~/.config/home-manager/flake.nix
+
+home-manager switch --flake ~/.config/home-manager/flake.nix 
