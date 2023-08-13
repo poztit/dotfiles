63abcb2d74a22b62d73c6538a09c6c11770fc97d
François Illien <francois@illien.org>
2023-08-13 22:32:21 +0200

    sdfsdff

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ unzip master.zip
 stow --dir dotfiles-master -S home-manager emacs
 
 nix-env -iA cachix -f https://cachix.org/api/v1/install && cachix use nix-community
-nix run home-manager/master --init --switch "$HOME/.config/home-manager/flake.nix"
-
+nix run home-manager/master -- init --switch "$HOME/.config/home-manager/flake.nix"
+nix run home-manager
 rm -rf master.zip dotfiles-master
