fce8d759ffdae5ed20c3d72ccb943d48dd932980
François Illien <francois@illien.org>
2023-08-13 23:09:35 +0200

    sdfsdfsd

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/bash
 
-curl -L https://github.com/poztit/dotfiles/archive/refs/heads/master.zip -o master.zip
-unzip master.zip
-
-stow --dir dotfiles-master -S home-manager emacs
-
+# Activate binary cache
 nix-env -iA cachix -f https://cachix.org/api/v1/install && cachix use nix-community
-nix run home-manager/master -- init --switch "$HOME/.config/home-manager/flake.nix"
+
+# Copy dotfiles
+curl -L https://github.com/poztit/dotfiles/archive/refs/heads/master.zip -o master.zip
+unzip master.zip -d $HOME/.dotfiles && rm -rf master.zip
+stow --dir .dotfiles -S home-manager emacs
+
+# Install dotfiles
 nix run home-manager switch
-rm -rf master.zip dotfiles-master
+
+# Disable the GNOME Keyring ssh agent
+mkdir -p $HOME/.config/autostart
+cp /etc/xdg/autostart/gnome-keyring-ssh.desktop ~/.config/autostart/
+echo "Hidden=true" >> $HOME/.config/autostart/gnome-keyring-ssh.desktop
+
+# Alert about chshing to zsh
+echo "To use ZSH you need to install it and change your default shell with `chsh -s /usr/bin/zsh`"
